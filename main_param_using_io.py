8c42213619a24795435c2da831c2310a905fe343
jangminhyuk <jason4012@snu.ac.kr>
2024-10-14 17:32:55 +0900

    14OCT24

--- main_param_using_io.py
+++ main_param_using_io.py
@@ -130,15 +130,15 @@ def main(dist, noise_dist, num_sim, num_samples, num_noise_samples, T):
     
     if dist=='normal':
         theta_v_list = [ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0] # radius of noise ambiguity set
-        theta_w_list = [0.1, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0] # radius of noise ambiguity set
-        theta_v_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0] # radius of noise ambiguity set
-        theta_w_list = [1.0, 2.0, 3.0, 4.0, 5.0] # radius of noise ambiguity set
-        theta_w_list = [6.0]
+        theta_w_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0] # radius of noise ambiguity set
+        #theta_v_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0] # radius of noise ambiguity set
+        #theta_w_list = [1.0, 2.0, 3.0, 4.0, 5.0] # radius of noise ambiguity set
+        #theta_w_list = [6.0]
     else:
         theta_v_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0] # radius of noise ambiguity set
-        theta_w_list = [0.2, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0] # radius of noise ambiguity set
-        theta_v_list = [1.0, 2.0, 3.0] # radius of noise ambiguity set
-        theta_w_list = [1.0, 2.0, 3.0] # radius of noise ambiguity set
+        theta_w_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0] # radius of noise ambiguity set
+        #theta_v_list = [1.0, 2.0, 3.0] # radius of noise ambiguity set
+        #theta_w_list = [1.0, 2.0, 3.0] # radius of noise ambiguity set
     lambda_list = [3] # disturbance distribution penalty parameter # not used if use_lambda = False
     theta_w = 1.0 # will not be used if use_lambda = True
     #num_x0_samples = 15 #  N_x0 
